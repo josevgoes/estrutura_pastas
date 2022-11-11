0c24debafeaf6da6828df8a6af47d9d93d1ecb29
Jose Goes <jose.goes@kroton.com.br>
2022-11-11 11:47:12 -0300

    FEATURE argsparse online

--- 03. Scripts/main.py
+++ 03. Scripts/main.py
@@ -1,25 +1,32 @@
+import os
+import argparse
 
-class EstruturaPastas:
+parser = argparse.ArgumentParser(description='Criação automática de pasta de estrutura de pastas')
+
+parser.add_argument('-d','--diretorio', type=str, help='Diretório que a estrutura será criada')
+parser.add_argument('-l','--lista_pastas_primarias', type=str, help='Nome das pastas primárias que serão criadas', nargs='+')
+
+args = parser.parse_args()
+
+def cria_pastas(diretorio, lista_pastas):
+  
+    size = len(lista_pastas)
     
-    def cria_pastas(diretorio, lista_pastas):
+    for i in range(size):
         
-        import os
+        n_pasta = str(i).zfill(2)
         
-        size = len(lista_pastas)
+        os.mkdir(diretorio+'/'+n_pasta+' - '+lista_pastas[i])
         
-        for i in range(size):
+        lista_sub_pasta = ['00. Backup', '01. Input', '02. Output','03. Scripts', '04. Dashboards']
+        
+        for l in range(len(lista_sub_pasta)):
             
-            n_pasta = str(i).zfill(2)
-            
-            os.mkdir(diretorio+'/'+n_pasta+' - '+lista_pastas[i])
-            
-            lista_sub_pasta = ['00. Backup', '01. Input', '02. Output','03. Scripts', '04. Dashboards']
-            
-            for l in range(len(lista_sub_pasta)):
-                
-                os.mkdir(diretorio+'/'+n_pasta+' - '+lista_pastas[i]+'/'+lista_sub_pasta[l])
+            os.mkdir(diretorio+'/'+n_pasta+' - '+lista_pastas[i]+'/'+lista_sub_pasta[l])
             
 
 
-EstruturaPastas.cria_pastas(diretorio='C:/Users/jose.goes/desktop', 
-                            lista_pastas=['Pasta Qualquer'])
+if __name__ == '__main__':
+
+    cria_pastas(diretorio=args.diretorio, 
+                                lista_pastas=args.lista_pastas_primarias)
